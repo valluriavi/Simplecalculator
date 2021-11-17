b86b9021f2a1c8f12577f4826ec131acc2aa59c7
Valluri Avinash <67725161+valluriavi@users.noreply.github.com>
2021-11-17 15:15:17 +0530

    Add files via upload

---
+++ Simple_calculator.py
@@ -0,0 +1,7 @@
+# Simple Calculator #
+
+print('Welcome to simple calculator')
+a = int(input('Enter a number :'))
+b = int(input('Enter a number to be multiplied :'))
+c = a*b
+print('Answer = ', c)
